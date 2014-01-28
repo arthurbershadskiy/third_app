e7d88f51d481a434c4e661458e6e3833c6fb39a0
Arthur Bershadskiy <arthurbershadskiy@gmail.com>
2014-01-28 17:38:04 -0500

    Finishes updating users information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,7 @@ def create
 	user = User.find_by(email: params[:session][:email].downcase)
 	if user && user.authenticate(params[:session][:password])
 	  sign_in user
-		redirect_to user
-		flash[:success] = 'Welcome'
+		redirect_back_or user
 	else
 		flash.now[:error] = 'Invalid username or password'
 		render 'new'
